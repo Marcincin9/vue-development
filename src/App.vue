878a116ea37a7ac95fad219bec9e62df2baec2cb
Marcincin9 <marcin.ols@yahoo.com>
2021-06-15 22:07:35 +0100

    add newFriend page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,137 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+        <header>
+          <h1>My Friends</h1>
+          </header>
+          <new-friend @add-contact="addContact"></new-friend>
+        <ul>
+           <friend-contact
+                v-for="friend in friends"
+                :key="friend.id"
+                :id="friend.id"
+                :name="friend.name"
+                :phone-number="friend.phone"
+                :email-address="friend.email">
+           </friend-contact>
+        </ul>
+    </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+        friends: [
+            {
+                id: 'robert',
+                name: 'Robert Naboj',
+                phone: '0748 000 111',
+                email: 'robertn@localhost.com'
+
+            },
+            {
+                id: 'grzegorz',
+                name: 'Grzegorz Grycar',
+                phone: '0748 000 222',
+                email: 'grzegorzg@localhost.com'
+
+            }
+        ]
+    };
+  },
+  methods: {
+    addContact(name, phone, email){
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phone: phone,
+        email: email
+      };
+      this.friends.push(newFriendContact);
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
 }
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li,
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #4994e9;
+  background-color: #4994e9;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #4a4757;
+  border-color: #4a4757;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  padding: 0.15rem;
+  font: inherit;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+
 </style>
+
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+    <li>
+        <h2>{{name}}</h2>
+        <button @click="toggleDetails">Show Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{phoneNumber}}</li>
+            <li><strong>Email:</strong>{{emailAddress}}</li>
+        </ul>
+    </li>
+</template>
+<script>
+export default {
+    props: [
+        'name',
+        'phoneNumber',
+        'emailAddress'
+    ],
+  data(){
+      return {
+          detailsAreVisible: false,
+           friend:{
+                id: 'robert',
+                name: 'Robert Naboj',
+                phone: '0748 000 111',
+                email: 'robertn@localhost.com'
+
+            },
+      };
+  },
+  methods: {
+      toggleDetails(){
+          this.detailsAreVisible = !this.detailsAreVisible;
+        
+        }
+  }
+};
+</script>
